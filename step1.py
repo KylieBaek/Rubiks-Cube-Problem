3eb8ac97e5a39691610b5b8bec31cd57bd31bf67
kyliebaek <chris8w8@naver.com>
2020-12-09 19:03:38 +0900

    Implement function rotates left

--- step1.py
+++ step1.py
@@ -1,5 +1,30 @@
 # coding=utf-8
 # 1단계- 단어 밀어내기 구현
+from collections import deque
+
+
+# 방향에 따라서 함수 구현
+def rotate_left():
+    global n, word
+    q = deque(list(word))
+
+    if n == len(word):
+        print(word)
+        return
+    elif n > len(word):
+        n = n % len(word)
+
+    while n > 0:
+        n -= 1
+        alpha = q.popleft()
+        q.append(alpha)
+
+    q = list(q)
+    res = ''
+
+    for x in q:
+        res += x
+    print(res)
 
 
 if __name__ == "__main__":
@@ -12,3 +37,5 @@ if __name__ == "__main__":
         n = int(n.split('-')[1])
     else:
         n = int(n)
+
+    rotate_left()
