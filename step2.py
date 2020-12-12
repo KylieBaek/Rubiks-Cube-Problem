e01fd26846d0e1f998d909d2127878b765fe2b03
kyliebaek <chris8w8@naver.com>
2020-12-12 19:01:55 +0900

    Implement full direction rotation

--- step2.py
+++ step2.py
@@ -4,39 +4,37 @@ from collections import deque
 import copy
 
 
-def rotate_left(arr):
+def rotate_left(arr, n):
     arr = copy.deepcopy(arr)
     q = deque(arr)
-    # 왼쪽으로 한 칸 밀기
-    alpha = q.popleft()
-    q.append(alpha)
+    # 왼쪽으로 밀기
+    while n > 0:
+        n -= 1
+        alpha = q.popleft()
+        q.append(alpha)
 
     q = list(q)
     return q
 
 
-def rotate_right(arr):
+def rotate_right(arr, n):
     stack = copy.deepcopy(arr)
-    # 오른쪽으로 한 칸 밀기
-    alpha = stack.pop()
-    stack.insert(0, alpha)
+    # 오른쪽으로 밀기
+    while n > 0:
+        n -= 1
+        alpha = stack.pop()
+        stack.insert(0, alpha)
 
     return stack
 
 
-# def rotate_list(arr, direction):
-#     if direction == 'l':
-#         result = rotate_left(arr)
-#     elif direction == 'r':
-#         result = rotate_right(arr)
-#     return result
-
-def print_graph():
-    global g
+def print_graph(inp):
+    print(inp)
     for k in range(3):
         for m in range(3):
             print(g[k][m], end=' ')
         print()
+    print()
 
 
 if __name__ == "__main__":
@@ -47,33 +45,59 @@ if __name__ == "__main__":
         g[i][0], g[i][1], g[i][2] = map(str, input().split())
 
     # 처음 배열 출력
-    for i in range(3):
-        for j in range(3):
-            print(g[i][j], end=' ')
-        print()
+    print_graph("")
 
     # CUBE 입력 받기
     while True:
-
         res = input('CUBE> ')
-        if res == "Q":
+        # 종료 조건
+        if res.startswith("Q"):
             print("Bye~")
             break
 
         # 회전할 리스트 받기
-        # '이 문자에 따라 chk 변수 만들어야함
-        for x in res:
-            input_arr = []
-            output_arr = []
+        res = list(map(str, res.split()))
+        input_arr = []
+        output_arr = []
 
+        for x in res:
             # 가장 윗줄 회전
             if x == "U" or x == "U'":
                 input_arr = g[0]
-                print(input_arr)
                 if x.endswith("'"):
-                    output_arr = rotate_right(input_arr)
+                    output_arr = rotate_right(input_arr, 1)
                 else:
-                    output_arr = rotate_left(input_arr)
-                print(output_arr)
+                    output_arr = rotate_left(input_arr, 1)
                 g[0] = output_arr
-                print_graph()
+
+            # 가장 아랫줄 회전
+            elif x == "B" or x == "B'":
+                input_arr = g[2]
+                if x.endswith("'"):
+                    output_arr = rotate_left(input_arr, 1)
+                else:
+                    output_arr = rotate_right(input_arr, 1)
+                g[2] = output_arr
+
+            # 가장 오른쪽 줄 회전
+            elif x == "R" or x == "R'":
+                input_arr = [g[0][2], g[1][2], g[2][2]]
+                if x.endswith("'"):
+                    output_arr = rotate_right(input_arr, 1)
+                else:
+                    output_arr = rotate_left(input_arr, 1)
+                for j in range(3):
+                    g[j][2] = output_arr[j]
+
+            # 가장 왼쪽 줄 회전
+            elif x == "L" or x == "L'":
+                input_arr = [g[0][0], g[1][0], g[2][0]]
+                if x.endswith("'"):
+                    output_arr = rotate_left(input_arr, 1)
+                else:
+                    output_arr = rotate_right(input_arr, 1)
+                for j in range(3):
+                    g[j][0] = output_arr[j]
+
+            # 큐브 출력
+            print_graph(x)
