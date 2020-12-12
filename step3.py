ad9bd1e027da5c3834a1d9c4fac93aa8b01fdccf
kyliebaek <chris8w8@naver.com>
2020-12-12 20:39:35 +0900

    Implement a function for printing a cube

--- step3.py
+++ step3.py
@@ -2,6 +2,32 @@
 # 2단계- 루빅스 큐브 구현
 
 
+def print_graph():
+    # up 출력
+    for i in range(3):
+        print('      ', end='')
+        for j in range(3):
+            print(g[4][i][j], end='')
+        print()
+    print()
+
+    # left, front, right , back 출력
+    for i in range(3):
+        for k in (2, 0, 3, 1):
+            for j in range(3):
+                print(g[k][i][j], end='')
+            print('   ', end='')
+        print()
+
+    # down 출력
+    print()
+    for i in range(3):
+        print('      ', end='')
+        for j in range(3):
+            print(g[5][i][j], end='')
+        print()
+
+
 if __name__ == "__main__":
     # 큐브 초기 상태
     g = [[0] * 3 for _ in range(6)]
@@ -17,3 +43,6 @@ if __name__ == "__main__":
     g[4] = [['B', 'B', 'B'] for _ in range(3)]
     # Down
     g[5] = [['R', 'R', 'R'] for _ in range(3)]
+
+    # 초기 상태 큐브 출력
+    print_graph()
